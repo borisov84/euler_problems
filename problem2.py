1a86799a4cecc8eb1c96f16d8e6746f44a02f4c8
Anton <toxa100500@gmail.com>
2023-01-04 10:22:58 +0500

    Problem #2

---
+++ problem2.py
@@ -0,0 +1,19 @@
+def fib_sum():
+    sum = 0
+    first_dig = 1
+    sec_dig = 0
+    f_dig = 0
+    fibonachi = []
+    while(True):
+        f_dig = first_dig + sec_dig
+        if f_dig > 4000000: break
+        fibonachi.append(f_dig)
+        if f_dig%2 == 0:
+            sum += f_dig
+        sec_dig = first_dig
+        first_dig = f_dig
+
+    print(sum)
+
+if __name__ == '__main__':
+    fib_sum()
